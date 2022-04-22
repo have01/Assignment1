008104b1e29218327a20fb30bf1d98df288a49ee
have01 <have010101@gmail.com>
2022-04-22 17:05:49 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,51 +1,65 @@
-import React from 'react'
-import { useState } from 'react'
-import './App.css'
+import React from "react";
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-
-   const [Num1, setNum1] = useState(0);
-   const [Num2, setNum2] = useState(0);
-  const [result, setResult] = useState(0);
-   
-  
+  const [Num1, setNum1] = useState(null);
+  const [Num2, setNum2] = useState(null);
+  const [result, setResult] = useState(null);
 
   function add() {
     setResult(Number(Num1) + Number(Num2));
   }
   function subtract() {
     setResult(Number(Num1) - Number(Num2));
-  } function Multiply() {
-    setResult(Number(Num1) *Number(Num2));
-  } function Divide() {
+  }
+  function Multiply() {
+    setResult(Number(Num1) * Number(Num2));
+  }
+  function Divide() {
     setResult(Number(Num1) / Number(Num2));
   }
-  
-    return (
-      <> 
-      <div className="main_container">
-      <div className="container">
-            <div>
-              <input type="number" onChange={(e) => setNum1(e.target.value)} /> <br />
-              <input type="number" onChange={(e) => setNum2(e.target.value)} />
-            </div>
-            <div className='btn'>
-              <button onClick={add}>+</button>
-              <button onClick={subtract}>-</button>
-              <button onClick={Multiply}>*</button>
-              <button onClick={Divide}>/</button>
-            </div>
 
-            <div className='result'> <h1>{result}</h1></div>
+  function reset() {
+    setResult("");
+    setNum1("");
+    setNum2("");
+  }
+  return (
+    <>
+      <div className="main_container">
+        <div className="container">
+          <div>
+            <input
+              value={Num1}
+              type="number"
+              onChange={(e) => setNum1(e.target.value)}
+              placeholder="Enter number one"
+            />{" "}
+            <br />
+            <input
+              value={Num2}
+              type="number"
+              onChange={(e) => setNum2(e.target.value)}
+              placeholder="Enter number two"
+            />
+          </div>
+          <div className="btn">
+            <button onClick={add}>+</button>
+            <button onClick={subtract}>-</button>
+            <button onClick={Multiply}>*</button>
+            <button onClick={Divide}>/</button>
           </div>
 
-      </div>
-         
-      </>
-    
+          <div className="result">
+            <h1>{result}</h1>
+          </div>
 
-    
-  )
+          <button onClick={reset}>Reset</button>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
